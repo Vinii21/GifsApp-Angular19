d9a1aa48070ef0783ea90164c135d0f8a0987317
Vinicio Chavarría U <viniciochavarriaugalde@gmail.com>
2025-06-10 18:32:12 -0600

    Maquetado de página para busqueda de gifs, endpoint para traer los gifs que se buscan en el servicio, uso de rxjs para usar el método map

--- src/app/gifs/pages/searchPage/searchPage.component.ts
+++ src/app/gifs/pages/searchPage/searchPage.component.ts
@@ -1,9 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
+import { GifService } from '../../services/gif.service';
+import { Gif } from '../../interfaces/gif.interface';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './searchPage.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchPageComponent { }
+export default class SearchPageComponent {
+
+  gifservice = inject(GifService);
+  gifs = signal<Gif[]>([])
+
+  onSearch(query: string) {
+    this.gifservice.loadSearchGifs(query).subscribe((resp)=>{
+      this.gifs.set(resp)
+    });
+  }
+}
--- src/app/gifs/services/gif.service.ts
+++ src/app/gifs/services/gif.service.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment';
 import { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { GifMapper } from '../mappers/gif.mapper';
 import { Gif } from '../interfaces/gif.interface';
+import { map } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class GifService {
@@ -25,8 +26,26 @@ export class GifService {
     }).subscribe((resp)=>{
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
       this.trendingGifs.set(gifs);
-      //console.log(this.trendingGifs())
+      console.log({trending: this.trendingGifs()})
     })
   };
+
+  loadSearchGifs(query: string) {
+    return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+      params: {
+        api_key: environment.giphyApiKey,
+        limit: 20,
+        q: query
+      }
+    }).pipe(
+      map(({data})=> GifMapper.mapGiphyItemsToGifArray(data) )
+    );
+
+    /* .subscribe((resp)=>{
+      const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+      this.searchGifs.set(gifs);
+      console.log({serach: gifs});
+    }) */
+  }
 
 }
