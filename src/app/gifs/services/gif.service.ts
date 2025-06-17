772e648e674ce878998e0c89f04823fed586d5a0
Vinicio Chavarría U <viniciochavarriaugalde@gmail.com>
2025-06-16 21:20:20 -0600

    Guardar datos en localStorage mediante los efectos de las señales, y recuperar esos datos para asignarlos como valor inicial de una señal

--- src/app/gifs/services/gif.service.ts
+++ src/app/gifs/services/gif.service.ts
@@ -1,10 +1,20 @@
 import { HttpClient } from '@angular/common/http';
-import { computed, inject, Injectable, signal } from '@angular/core';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
+import { map, tap } from 'rxjs';
+
 import { environment } from '@environments/environment';
 import { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { GifMapper } from '../mappers/gif.mapper';
 import { Gif } from '../interfaces/gif.interface';
-import { map, tap } from 'rxjs';
+
+const GIF_KEY = "gifs";
+
+const loadGifsFromLocalStorage = (): Record<string, Gif[]> => {
+  // TODO: Sería valioso validar que efectivamente el tipo de dato sea el que se especifico, ya que los datos del localStorage se pueden modificar desde el lado del cliente con mucha facilidad.
+  const gifsFromLocalStorage = localStorage.getItem(GIF_KEY) ?? '{}';
+  const gifs: Record<string, Gif[]> = JSON.parse(gifsFromLocalStorage);
+  return gifs;
+}
 
 @Injectable({providedIn: 'root'})
 export class GifService {
@@ -12,13 +22,19 @@ export class GifService {
   private http = inject(HttpClient);
   trendingGifs = signal<Gif[]>([]);
 
-  searchHistory = signal<Record<string, Gif[]>>({});
+  searchHistory = signal<Record<string, Gif[]>>(loadGifsFromLocalStorage());
   // Cada que la señal searchHistry() cambie, se volverá a computar el searchHistoryKey()
   searchHistoryKey = computed(()=> Object.keys(this.searchHistory()));
 
   constructor () {
     this.loadTrendingGifs();
-  }
+  };
+
+  // Efecto de signal para almacenar el localStorage
+  saveLocalStorage = effect(()=>{
+    const historyJson = JSON.stringify(this.searchHistory());
+    localStorage.setItem(GIF_KEY, historyJson);
+  });
 
   loadTrendingGifs() {
     this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
